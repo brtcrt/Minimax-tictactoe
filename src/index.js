0f1d478c4f078bafc70442c31de751b59708bf52
brt crt <bartuturan05@gmail.com>
2021-03-27 12:39:00 +0300

    Stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,135 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+    render() {
+      return (
+        <button className="square" 
+        onClick={()=> {this.props.onClick()}}
+        >
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      let won = calculateWinner(squares);
+      if (won) return;
+      if(squares[i] === "O") return;
+      squares[i] = "X";
+      this.setState({squares: squares});
+      AutoPlay(squares);
+    }
+    renderSquare(i) {
+      return <Square value={this.state.squares[i]} onClick={()=> {this.handleClick(i)}}/>;
+    }
+  
+    render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner + " Refresh the page to play again.";
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function AutoPlay(squares) {
+  let won = calculateWinner(squares)
+  if (won) {
+    return;
+  }
+  console.log(squares);
+  let emptySquares = [];
+  for(let i=0; i<squares.length; i++) {
+    if (squares[i] === null) {
+      emptySquares.push(i);
+    }
+  }
+  const randomSquare = Math.floor(Math.random()*emptySquares.length);
+  console.log(randomSquare);
+  console.log(emptySquares);
+  squares[emptySquares[randomSquare]] = "O";
+  
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
